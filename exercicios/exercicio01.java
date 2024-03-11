1e0176f318587aa67e9b17310ec96a36300ab3ee
KAROLAYNE DA SILVA ALEXANDRE <karolayne.alexandre@univille.br>
2024-03-11 19:58:36 -0300

    exercicio01

---
+++ exercicios/exercicio01.java
@@ -0,0 +1,12 @@
+/*Escreva um programa em java que declare uma vari´avel do tipo inteiro.
+Atribua o maior valor poss´ıvel para essa vari´avel. Some mais 1 e apresente
+o resultado em tela. Observe e analise o resultado.*/
+
+public class exercicio01 {
+    public static void main ( String args []) {
+        int variavel =  Integer.MAX_VALUE;
+        int soma = variavel + 1;  
+        System . out . println (" A soma da variável com o número 1 é: "+soma) ;
+    }
+}
+    
