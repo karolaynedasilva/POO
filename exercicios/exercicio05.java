423851b4e8a38bfe7a4c100fd76a847ff095d2d4
KAROLAYNE DA SILVA ALEXANDRE <karolayne.alexandre@univille.br>
2024-03-12 21:06:07 -0300

    exercicio05

---
+++ exercicios/exercicio05.java
@@ -0,0 +1,9 @@
+/*Escreva um programa em java que declare uma vari´avel do tipo caracter.
+Atribui o valor inteiro 88 para a vari´avel apresente o resultado em tela.
+Observe e analise o resultado */
+public class exercicio05 {
+    public static void main(String[] args) {
+        char variavel = 88;
+        System.out.println("A variável tem o valor:"+ variavel);
+    }
+}
